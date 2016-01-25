bf2856d6e35fa91c4c791369f3c53bc2ffc19001
jordan <jordan.roimarmier@outlook.com>
2016-01-25 19:44:22 +0100

    Mise en place de MVC et du controlleur pour geter le HTML de météo france

---
+++ sources/controller/meteo/get.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+if(isset($_GET["city"]) AND $_GET["postalCode"]){
+    $city = $_GET["city"];
+    $postalCode = $_GET["postalCode"];
+    
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => "http://www.meteofrance.com/previsions-meteo-france/" . $city . "/" . $postalCode
+    ));
+    $result = curl_exec($curl);
+    echo($result);
+}
